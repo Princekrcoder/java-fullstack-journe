efd80cba050bce2916bd3116f906fc2a40ebaaef
Prince Kumar <princekrcoder@gmail.com>
2025-09-05 10:54:54 +0530

    Add files via upload

---
+++ 📁 java-basics/02-input-output-variables-datatypes/PrimitiveDataTypesDemo.java
@@ -0,0 +1,16 @@
+public  class PrimitiveDataTypesDemo {
+    public static void main(String[] args) {
+        // Primitive Data Types in Java
+        int intValue = 100;               // Integer (whole number)
+        double doubleValue = 99.99;       // Floating point number
+        char charValue = 'A';             // Single character
+        boolean booleanValue = true;      // true or false
+        String stringValue = "Hello";    // Sequence of characters (not primitive but commonly used)
+
+        // Print the values
+        System.out.println("Integer Value: " + intValue);
+        System.out.println("Double Value: " + doubleValue);
+        System.out.println("Character Value: " + charValue);
+        System.out.println("Boolean Value: " + booleanValue);
+    }
+}
