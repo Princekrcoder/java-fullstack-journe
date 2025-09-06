27e57c9e4dbe9a0054f6790af4094a6d6d1fb002
Prince Kumar <princekrcoder@gmail.com>
2025-09-06 23:37:33 +0530

    Add files via upload

---
+++ 📁 java-basics/04-loops/LoopExample.java
@@ -0,0 +1,7 @@
+public class LoopExample {
+    public static void main(String args[]) {
+        for(int counter = 0; counter < 5; counter++) {
+            System.out.println("Hello World! ");
+        }
+    }
+}
