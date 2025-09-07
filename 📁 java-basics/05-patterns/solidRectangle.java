70ac0fa1e5a84b48cfe5683d124de5bd85054a54
Prince Kumar <princekrcoder@gmail.com>
2025-09-07 18:11:01 +0530

    Add files via upload

---
+++ 📁 java-basics/05-patterns/solidRectangle.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class solidRectangle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+         System.out.print("Enter no. of rows : ");
+        int n = sc.nextInt();
+       
+
+        System.out.print("Enter no. of columns : ");
+        int m = sc.nextInt();
+        
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
